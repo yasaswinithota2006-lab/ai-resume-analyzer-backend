57d2046e8d6aecf2642ce8db492eaeae91c7ee54
Yashu <yasaswinithota2006@gmail.com>
2026-06-24 20:50:20 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/resume", require("./routes/resumeRoutes"));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.listen(5000, () => {
+    console.log("Server Running on Port 5000");
+});
